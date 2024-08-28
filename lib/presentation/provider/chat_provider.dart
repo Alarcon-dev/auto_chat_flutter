db44c26455b039e5d9354988126a9a5a688ddea3
Alarcon-dev <devalarcon.23@gmail.com>
2024-08-28 15:22:47 -0500

    se añadió archivo helpers, y el modelo

--- lib/presentation/provider/chat_provider.dart
+++ lib/presentation/provider/chat_provider.dart
@@ -1,8 +1,10 @@
 import "package:flutter/material.dart";
+import "package:yes_no_app/config/helpers/get_yes_no_answer.dart";
 import "package:yes_no_app/domain/entities/message.dart";
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getAnswer = GetYesNoAnswer();
 
   List<Message> messages = [
     Message(text: 'Hello beatiful instructor', fromWho: FromWho.me),
@@ -11,8 +13,13 @@ class ChatProvider extends ChangeNotifier {
 
   Future<void> sendMessage(String text) async {
     if (text.isEmpty) return;
+
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messages.add(newMessage);
+
+    if (text.endsWith('?')) {
+      yourAnswer();
+    }
 
     notifyListeners();
     moveScroll();
@@ -25,5 +32,13 @@ class ChatProvider extends ChangeNotifier {
         chatScrollController.position.maxScrollExtent,
         duration: const Duration(milliseconds: 300),
         curve: Curves.easeOut);
+  }
+
+  Future<void> yourAnswer() async {
+    final yourMessage = await getAnswer.getAnswer();
+
+    messages.add(yourMessage);
+    notifyListeners();
+    moveScroll();
   }
 }
